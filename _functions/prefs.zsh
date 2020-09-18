595287561a1c1df21ecfe63cd1c5127845f6546e
Jorhel Bourdier <jorhel.bdr@gmail.com>
2020-09-18 23:49:47 +0200

    Prefs

--- _functions/prefs.zsh
+++ _functions/prefs.zsh
@@ -333,7 +333,7 @@ defaults write com.apple.dock autohide-time-modifier -float 0
 defaults write com.apple.dock autohide -bool false
 
 # Make Dock icons of hidden applications translucent
-defaults write com.apple.dock showhidden -bool true
+defaults write com.apple.dock showhidden -bool false
 
 # Don’t show recent applications in Dock
 defaults write com.apple.dock show-recents -bool false
